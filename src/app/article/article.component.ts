c410c261817c62feddfd0f07b7b0e6efbd7225f3
Mithlesh Dhore <MithleshD@talentica-all.com>
2017-04-07 19:29:39 +0530

    Added multiple article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -10,22 +10,15 @@ import { Article } from './article.model';
   }
 })
 export class ArticleComponent implements OnInit {
-  article: Article;
-
-  constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10);
-  }
+@Input() article: Article;
 
   voteUp(): boolean {
-    this.article.votes += 1;
+    this.article.voteUp();
     return false;
   }
 
   voteDown(): boolean {
-    this.article.votes -= 1;
+    this.article.voteDown();
     return false;
   }
 
